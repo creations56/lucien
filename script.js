4c1e9ed95d32ec6219b7855040404c778f395405
creations56 <74179508+creations56@users.noreply.github.com>
2026-08-20 16:11:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const hour=document.getElementById("hour"),minute=document.getElementById("minute"),smallSecond=document.getElementById("smallSecond"),dateNumber=document.getElementById("dateNumber"),ampm=document.getElementById("ampm");
+function updateClock(){const now=new Date();dateNumber.textContent=now.getDate();ampm.textContent=now.getHours()<12?"AM":"PM";const s=now.getSeconds()+now.getMilliseconds()/1000,m=now.getMinutes()+s/60,h=(now.getHours()%12)+m/60;
+hour.style.transform=`rotate(${h*30}deg)`;minute.style.transform=`rotate(${m*6}deg)`;smallSecond.style.transform=`rotate(${s*6}deg)`;requestAnimationFrame(updateClock)}
+updateClock();
